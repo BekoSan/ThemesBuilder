8f6049fddb87073c813d243588157679949a394c
Abubakr Mahdi <abubakrmahdi20@gmail.com>
2019-10-17 09:30:31 +0200

    Add project files.

---
+++ HatusmeiThemesBuilderLibrary/IThemeFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatusmeiThemesBuilderLibrary
+{
+    /// <summary>
+    /// Main apstraction interface for every theme.
+    /// </summary>
+    public interface IThemeFactory
+    {
+        Color ButtonBackColor { get; set; }
+        Color ButtonForeColor { get; set; }
+        Color BackgroundColor { get; set; }
+        Font HeadersFont { get; set; }
+        Font ButtonsFont { get; set; }
+    }
+
+}
---
+++ HatusmeiThemesBuilderLibrary/ThemesManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HatusmeiThemesBuilderLibrary
+{
+
+    /// <summary>
+    /// Manage themes and how to apply them.
+    /// </summary>
+   public static  class ThemesManager
+    {
+
+        /// <summary>
+        /// The default theme for the application.
+        /// </summary>
+        public static IThemeFactory AppTheme { get; private set; }
+
+        /// <summary>
+        /// Initalize theme for your application.
+        /// </summary>
+        /// <param name="theme">The theme you want to apply.</param>
+        public static void InitializeTheme(IThemeFactory theme)
+        {
+            AppTheme = theme;
+        }
+
+        /// <summary>
+        /// Apply the theme in the form and all its controls.
+        /// Note : it just works for three levels of controls ie.(ParentControl=>ChildControls=>ChildChildControls).
+        /// </summary>
+        /// <param name="form">The form want to apply theme.</param>
+        public static void SetupUI(Form form)
+        {
+            form.BackColor = AppTheme.BackgroundColor;
+            SetupControl(form);
+            foreach (Control control in form.Controls)
+            {
+                SetupControl(control);
+                foreach (Control c in control.Controls)
+                {
+                    SetupControl(c);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Apply the default app theme into control and its child controls.
+        /// </summary>
+        /// <param name="control">The control you want to apply theme on.</param>
+        private static void SetupControl(Control control)
+        {
+            if (control.HasChildren == false) return;
+
+            foreach (Control subControl in control.Controls)
+            {
+                switch (subControl.GetType().ToString())
+                {
+                    case "System.Windows.Forms.Button":
+                        subControl.GetType().GetProperty("ForeColor").SetValue(subControl, AppTheme.ButtonForeColor);
+                        subControl.GetType().GetProperty("BackColor").SetValue(subControl, AppTheme.ButtonBackColor);
+                        subControl.GetType().GetProperty("Font").SetValue(subControl, AppTheme.ButtonsFont);
+                        break;
+                    case "System.Windows.Forms.Lable":
+                        subControl.GetType().GetProperty("ForeColor").SetValue(subControl, AppTheme.ButtonForeColor);
+                        subControl.GetType().GetProperty("BackColor").SetValue(subControl, AppTheme.ButtonBackColor);
+                        break;
+                    case "System.Windows.Forms.DataGridView":
+                        ((DataGridView)subControl).ColumnHeadersDefaultCellStyle.BackColor = AppTheme.ButtonBackColor;
+                        ((DataGridView)subControl).ColumnHeadersDefaultCellStyle.ForeColor = AppTheme.ButtonForeColor;
+                        ((DataGridView)subControl).BackgroundColor = AppTheme.BackgroundColor;
+                        break;
+                    case "System.Windows.Forms.GroupBox":
+                        subControl.GetType().GetProperty("ForeColor").SetValue(subControl, AppTheme.ButtonForeColor);
+                        subControl.GetType().GetProperty("Font").SetValue(subControl, AppTheme.HeadersFont);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+
+}
